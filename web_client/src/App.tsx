ad0c4e0267fba069b598bdb9cc53d8c9097b1e51
Arjan <arjanpingale@gmail.com>
2026-01-16 17:14:47 +0530

    more UI work

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -5,8 +5,9 @@ function App() {
   const { id } = useSignaling();
   return (
     <>
-      <h1>Client ID: {id}</h1>
       <div className="h-[100dvh] flex flex-col items-center bg-gray-800 overflow-y-auto">
+
+        <h1>Client ID: {id}</h1>
         <main className="h-full w-full p-4">
           <div className='w-full h-full px-4 rounded-xl shadow-xl bg-gray-600'>
             <Outlet />
