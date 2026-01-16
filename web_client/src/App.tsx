5082a96ef4aa0b4b451a4a96f96eb976671eca42
Arjan <arjanpingale@gmail.com>
2026-01-16 23:18:18 +0530

    add y scrolling

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <>
       <div className="h-[100dvh] flex flex-col items-center bg-gray-800 overflow-y-auto">
-        <main className="h-full w-full p-4 overflow-hidden">
-          <div className='w-full h-full px-4 rounded-xl shadow-xl bg-gray-600 overflow-hidden'>
+        <main className="h-full w-full p-4 overflow-y-auto">
+          <div className='w-full h-full px-4 rounded-xl shadow-xl bg-gray-600 overflow-y-auto'>
             <Outlet />
           </div>
         </main>
