dbabf85f41cd56090b213239ef9db86f31d9cd35
Arjan <arjanpingale@gmail.com>
2026-01-16 17:16:17 +0530

    UI rework

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -6,9 +6,8 @@ function App() {
   return (
     <>
       <div className="h-[100dvh] flex flex-col items-center bg-gray-800 overflow-y-auto">
-
-        <h1>Client ID: {id}</h1>
         <main className="h-full w-full p-4">
+          <h1>Client ID: {id}</h1>
           <div className='w-full h-full px-4 rounded-xl shadow-xl bg-gray-600'>
             <Outlet />
           </div>
